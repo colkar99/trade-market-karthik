0a993f53cb33aca6384f2da4c92770be238c18b2
karthikraj <colkar99@gmail.com>
2020-05-31 01:40:37 +0530

    night work

--- Metadata/metadata.js
+++ Metadata/metadata.js
@@ -1,6 +1,12 @@
 // let {candles} = require('./candleData');
 let finalDatas = [];
 let candleData = dataFromKite.data;
+let tempDate = 0;
+//custom range 
+let range = 0.15;
+let customProfit = 0;
+let whichCandle = null;
+firstCandle = {date: '', open: 0, high: 0, low: 0, close: 0, volume: 0, type: '' };
 // var outputData = {
 //     0: {
 //         instrumentName: 'infy',
@@ -64,16 +70,99 @@ async function start() {
     let date = new Date(candle[0]);
     let meataData = await getMetaData(date.getMonth());
     // console.log(meataData);
-    mainLogicFun(meataData, candle);
+    initializeEngine(meataData, candle);
     // console.log(date.getMonth());
   }
 }
 start();
 
-function mainLogicFun(meta, candle) {
-  console.log("Meta:", meta);
-  console.log("Candle:", candle);
+function initializeEngine(meta, candle) {
 
+  let date = new Date(candle[0]).getDate();
+
+  if (tempDate != date) {
+    tempDate = date;
+    initializeFirstCandle(candle);
+    whichCandle = "start";
+  } else if (tempDate === date) {
+    //Main logic goes here
+    let currentCandle = { open: candle[1], high: candle[2], low: candle[3], close: candle[4], volume: candle[5] };
+    let difference;
+    if (whichCandle != null) {
+      if (firstCandle.type == 'Bullesh') {
+        difference = firstCandle.close - currentCandle.open;
+        if(range < difference){
+          startTrade(meta,currentCandle);
+          meta.noOfTrades ++;
+        }
+        console.log("Current Candle:",currentCandle)
+        console.log("Difference Amount:",difference)
+      }
+      else if(firstCandle.type == 'Bearesh'){
+        difference = firstCandle.close - currentCandle.open;
+        console.log("Current Candle:",currentCandle)
+        console.log("Difference Amount:",difference)
+      }
+      whichCandle = null;
+    }
+
+
+  }
+  // console.log("Meta:", meta);
+  // console.log("Candle:", candle);
+}
+
+
+
+//Start trade function
+function startTrade(meta,currentCandle){
+  debugger
+  meta.noOfTrades ++;
+  let stopLossType = firstCandle.type == 'Bullesh'? firstCandle.low : firstCandle.high;
+  let  data = {
+    date: firstCandle.date,
+    type: firstCandle.type,
+    entry: currentCandle.open,
+    stop: stopLossType,
+    stopPercentage: stopLossPercentage(currentCandle.open,stopLossType),
+    closePrice: 0,
+    exitprice: 0,
+    profit: customProfit || 1,
+    takeProfit: takeProfit(),
+    pointsEarnedAndLoss: takeProfitFunction(customProfit || 1,currentCandle.open),
+  }
+  function takeProfit(){
+    let profit =  takeProfitFunction(customProfit || 1,currentCandle.open);
+    return currentCandle.open + profit;
+  }
+  function stopLossPercentage(val1,val2){
+    let first = val1 - val2;
+    let second = val1 + val2;
+    let third = second / 2;
+    let fourth = first / third;
+    let fifth = fourth * 100;
+    return fifth;
+  }
+  function takeProfitFunction(per,val){
+    return (val/100)*per;
+  }
+
+}
+
+//First candle initialize open close bullesh
+function initializeFirstCandle(candle) {
+  firstCandle.open = candle[1];
+  firstCandle.close = candle[4];
+  firstCandle.high = candle[2];
+  firstCandle.low = candle[3];
+  firstCandle.volume = candle[5];
+  firstCandle.date = candle[0]
+  if (firstCandle.close > firstCandle.open) firstCandle.type = 'Bullesh';
+  else if (firstCandle.close < firstCandle.open) firstCandle.type = 'Bearesh';
+  else firstCandle.type = 'skipped';
+  console.log(tempDate);
+  // console.log("first candle:", candle)
+  console.log("first candle our:", firstCandle)
 }
 
 function getMetaData(month) {
@@ -143,7 +232,7 @@ function genrateMetaData(month) {
         pointsEarnedAndLoss: 0,
       },
     ],
-    touched = false;
+    touched: false
   };
 }
 
