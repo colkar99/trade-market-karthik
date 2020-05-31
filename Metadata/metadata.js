08f545e6209dfa0f351b13159697f92b4b71c1c4
colkar99 <colkar99@gmail.com>
2020-05-31 15:34:37 +0530

    asdasd

--- Metadata/metadata.js
+++ Metadata/metadata.js
@@ -1,10 +1,14 @@
 // let {candles} = require('./candleData');
+let dateRef = 0
+
 let finalDatas = [];
 let candleData = dataFromKite.data;
 let tempDate = 0;
 //custom range
 let range = 0.15;
 let customProfit = 1;
+let customStopLossPercentage = null;
+let customQuantity = 100;
 let timeFrame = 30;
 let timeFrameCal = { 30: 13 };
 let tempFrame = null;
@@ -29,23 +33,32 @@ let currentCandle = {
 };
 let coreData = {
   date: "",
-  type: "",
+  type: null,
   entry: 0,
   stop: 0,
   stopPercentage: 0,
   closePrice: 0,
-  exitprice: 0,
+  exitprice: null,
   profit: 0,
   takeProfit: 0,
   pointsEarnedAndLoss: 0,
-  exitTriggerdTime: ''
+  exitTriggerdTime: '',
+  commets: ''
 };
 
-async function start() {
+ function start() {
+  let monthNow = null;
+  let meataData;
   for (let i = 0; i < candleData.candles.length; i++) {
     let candle = candleData.candles[i];
     let date = new Date(candle[0]);
-    let meataData = await getMetaData(date.getMonth());
+    if(monthNow == null){
+      monthNow = date.getMonth();
+      meataData =  getMetaData(date.getMonth());
+    }else if(monthNow != date.getMonth()){
+      monthNow = date.getMonth();
+      meataData =  getMetaData(date.getMonth());
+    }
     // console.log(meataData);
     initializeEngine(meataData, candle);
     // console.log(date.getMonth());
@@ -55,7 +68,7 @@ start();
 
 function initializeEngine(meta, candle) {
   let date = new Date(candle[0]).getDate();
-
+  dateRef = date;
   if (tempDate != date) {
     tempDate = date;
     tempFrame = timeFrameCal[timeFrame];
@@ -64,6 +77,7 @@ function initializeEngine(meta, candle) {
     tempFrame--;
   } else if (tempDate === date) {
     tempFrame--;
+
     //Main logic goes here
     currentCandle.date = candle[0]
     currentCandle.open = candle[1];
@@ -75,16 +89,16 @@ function initializeEngine(meta, candle) {
     let difference;
     if (whichCandle != null) {
       if (firstCandle.type == "Bullesh") {
-        difference = firstCandle.close - currentCandle.open;
-        if (range < difference) {
+        difference = currentCandle.open - firstCandle.close;
+        if (difference >= 0 || difference >= -range) {
           startTrade(meta, currentCandle);
-          meta.noOfTrades++;
+          // meta.noOfTrades++;
         }
       } else if (firstCandle.type == "Bearesh") {
         difference = firstCandle.close - currentCandle.open;
-        if (range > difference) {
+        if (difference >= 0 || difference >= -range) {
           startTrade(meta, currentCandle);
-          meta.noOfTrades++;
+          // meta.noOfTrades++;
         }
       }
       whichCandle = null;
@@ -94,11 +108,19 @@ function initializeEngine(meta, candle) {
       tempDate = 0;
       tempFrame = null;
       whichCandle = null;
-      coreConcept(meta, "true");
-      // resetCoreDate();
+
+      if(coreData.entry != 0 && coreData.exitprice == null){
+        coreConcept(meta, "true");
+      }
+      coreData.date = currentCandle.date;
+      meta.noOfDays = meta.data.length + 1;
+      meta.data.push(coreData);
       console.log("First candle: ", firstCandle);
       console.log("Last candle: ", currentCandle);
       console.log("Core data : ", coreData);
+      console.log("MetaData : ", finalDatas);
+
+      resetCoreDate();
     } else {
       coreConcept(meta);
     }
@@ -109,8 +131,9 @@ function initializeEngine(meta, candle) {
 
 //Start Core concept
 function coreConcept(meta, squareOff = null) {
+  // if(dateRef == 9) debugger
+
   //Checking stopLoss start
-  debugger
   if (coreData.type == "Bullesh") {
     if (coreData.stop - currentCandle.low >= 0) {
       buyOrSell(meta, coreData.stop, "stopLossHitted");
@@ -123,7 +146,7 @@ function coreConcept(meta, squareOff = null) {
     if (currentCandle.high - coreData.stop >=0) {
       //Stoploss hit
       buyOrSell(meta, coreData.stop, "stopLossHitted");
-    } else if (coreData.takeProfit - currentCandle.low >= 0) {
+    } else if (currentCandle.low - coreData.takeProfit  <= 0) {
       //Target hit
       buyOrSell(meta, coreData.takeProfit, "targetHitted");
     }
@@ -135,26 +158,108 @@ function coreConcept(meta, squareOff = null) {
 
 //Common functin for price calculation of evert candle
 function buyOrSell(meta, exitPrice, which) {
-  if (coreData.exitprice <= 0) {
-    coreData.exitprice = exitPrice;
-    coreData.exitTriggerdTime = currentCandle.date;
-    console.log("profit_or_loss:", which);
-  } else {
+  if (coreData.exitprice == null) {
+    if(which == 'squareOff'){
+      coreData.exitprice = exitPrice;
+      coreData.exitTriggerdTime = currentCandle.date;
+      coreData.pointsEarnedAndLoss = coreData.entry - exitPrice;
+      coreData.commets = "May be squareof please check"
+      //
+      if(coreData.pointsEarned > 0){
+        meta.maxProfitOneTrade = Math.max(meta.maxProfitOneTrade,coreData.pointsEarnedAndLoss)
+        meta.noOfWinningTrades++
+      }
+      else{
+        meta.maxLossOneTrade = Math.min(meta.maxProfitOneTrade,coreData.pointsEarnedAndLoss)
+        meta.noOfLosingTrades++;
+      } 
+      meta.squareOfHit++
+
+      console.log("profit_or_loss:", which);
+    }else if(which == 'targetHitted'){
+      coreData.exitprice = exitPrice;
+      coreData.exitTriggerdTime = currentCandle.date;
+      //
+      meta.maxProfitOneTrade = Math.max(meta.maxProfitOneTrade,coreData.pointsEarnedAndLoss)
+      meta.noOfWinningTrades++
+      meta.targetHit++;
+      console.log("profit_or_loss:", which);
+    }
+     else if(which == 'stopLossHitted') {
+      coreData.exitprice = exitPrice;
+      coreData.exitTriggerdTime = currentCandle.date;
+      //
+      meta.maxLossOneTrade = Math.min(meta.maxProfitOneTrade,coreData.pointsEarnedAndLoss)
+      meta.noOfLosingTrades++;
+      meta.stoplossHit++;
+      console.log("profit_or_loss:", which);
+    }
+
+    //
+    meta.pointsEarned = meta.pointsEarned + coreData.pointsEarnedAndLoss;
+    meta.netProfitAndLoss = meta.quantity * meta.pointsEarned;
+    meta.maxLossPoints = meta.maxLossOneTrade * meta.quantity;
+    meta.maxProfitPoints = meta.maxProfitOneTrade * meta.quantity;
+    meta.noOfDays = meta.data.length;
+    meta.customProfit = customProfit || 1;
+    meta.partialLoss = meta.noOfLosingTrades - meta.stoplossHit;
+    meta.partialProfit = meta.noOfWinningTrades - meta.targetHit;
+    meta.averagePoints = meta.pointsEarned / meta.noOfTrades;
+
   }
 }
-
+// squareOffFunction implementation
+function squareOffFunction(exitPrice){
+  debugger
+  if(coreData.type == 'Bullesh'){
+    let diff;
+    if(coreData.entry >= exitPrice ){
+        diff = stopLossPercentage(coreData.entry,exitPrice);
+    }
+    else{
+      diff = stopLossPercentage(exitPrice , coreData.entry);
+    }
+    console.log(diff);
+  }
+    // meta.noOfTrades++;
+    // coreData.date = firstCandle.date;
+    // coreData.type = firstCandle.type;
+    // coreData.entry = currentCandle.open;
+    // coreData.stop = stopLossType;
+    // coreData.stopPercentage = stopLossPercentage(coreData.entry, coreData.stop);
+    // coreData.closePrice = 0;
+    // coreData.exitprice = null;
+    // coreData.profit = customProfit || 1;
+    // coreData.pointsEarnedAndLoss = takeProfitFunction(
+    //   coreData.profit,
+    //   coreData.entry
+    // );
+    // coreData.takeProfit = coreData.type == 'Bullesh'? coreData.entry + coreData.pointsEarnedAndLoss:coreData.entry - coreData.pointsEarnedAndLoss;
+}
 //Start trade function
 function startTrade(meta, currentCandle) {
+
+  let stopLossType = firstCandle.type == "Bullesh" ? firstCandle.low : firstCandle.high;
+  let stopLossPercent = stopLossPercentage(currentCandle.open, stopLossType);
+  if(customStopLossPercentage == null ){
+    initCore(meta,stopLossType);
+  }else{
+    if(stopLossPercent <= customStopLossPercentage){
+      initCore(meta,stopLossType);
+    }else return
+  }
+
+}
+//Common for above function
+function initCore(meta,stopLossType){
   meta.noOfTrades++;
-  let stopLossType =
-    firstCandle.type == "Bullesh" ? firstCandle.low : firstCandle.high;
   coreData.date = firstCandle.date;
   coreData.type = firstCandle.type;
   coreData.entry = currentCandle.open;
   coreData.stop = stopLossType;
   coreData.stopPercentage = stopLossPercentage(coreData.entry, coreData.stop);
   coreData.closePrice = 0;
-  coreData.exitprice = 0;
+  coreData.exitprice = null;
   coreData.profit = customProfit || 1;
   coreData.pointsEarnedAndLoss = takeProfitFunction(
     coreData.profit,
@@ -165,21 +270,36 @@ function startTrade(meta, currentCandle) {
 
 //Reset core data
 function resetCoreDate() {
-  if (coreData.exitprice == 0) {
-    coreData.exitprice = currentCandle.close;
-    coreData.closePrice = currentCandle.close;
-  }
+  firstCandle = {
+    date: "",
+    open: 0,
+    high: 0,
+    low: 0,
+    close: 0,
+    volume: 0,
+    type: "",
+  };
+  currentCandle = {
+    date: '',
+    open: 0,
+    high: 0,
+    low: 0,
+    close: 0,
+    volume: 0,
+    type:''
+  };
   coreData = {
     date: "",
-    type: "",
+    type: null,
     entry: 0,
     stop: 0,
     stopPercentage: 0,
     closePrice: 0,
-    exitprice: 0,
+    exitprice: null,
     profit: 0,
     takeProfit: 0,
     pointsEarnedAndLoss: 0,
+    exitTriggerdTime: ''
   };
 }
 
@@ -206,25 +326,28 @@ function takeProfit() {
 function stopLossPercentage(val1, val2) {
   return 100 * Math.abs((val1 - val2) / ((val1 + val2) / 2));
 }
+
+
+console.log('custom loss calculator',stopLossPercentage(609,602));
 function takeProfitFunction(per, val) {
   return (val / 100) * per;
 }
 
 function getMetaData(month) {
-  if (finalDatas.length == 0) {
+  // if (finalDatas.length == 0) {
     let temp = genrateMetaData(month);
     finalDatas.push(temp);
     return temp;
-  } else {
-    for (let i = 0; i < finalDatas.length; i++) {
-      let temp = finalDatas[i];
-      temp.noOfTrades++;
-      if (temp.monthNumber == month) return temp;
-      else {
-        return new Error("Please check this line if you this Error");
-      }
-    }
-  }
+  // } else {
+  //   for (let i = 0; i < finalDatas.length; i++) {
+  //     let temp = finalDatas[i];
+  //     // temp.noOfTrades++;
+  //     if (temp.monthNumber == month) return temp;
+  //     else {
+  //       return new Error("Please check this line if you this Error");
+  //     }
+  //   }
+  // }
 }
 
 function genrateMetaData(month) {
@@ -240,42 +363,19 @@ function genrateMetaData(month) {
     noOfWinningTrades: 0,
     noOfLosingTrades: 0,
     pointsEarned: 0,
-    quantity: 0,
-    netProfitAndLoss: this.pointsEarned * this.quantity,
+    quantity: customQuantity,
+    netProfitAndLoss: 0,
     maxProfitPoints: 0,
     maxLossPoints: 0,
-    maxProfitOneTrade: this.quantity * this.maxProfitPoints,
-    maxLossPoints: this.quantity * this.maxLossPoints,
+    maxProfitOneTrade: 0,
+    maxLossOneTrade: 0,
     targetHit: 0,
     stoplossHit: 0,
-    partialProfit: this.noOfWinningTrades - this.targetHit,
-    partialLoss: this.noOfLosingTrades - this.stoplossHit,
-    averagePoints: this.pointsEarned / this.noOfTrades,
+    squareOfHit: 0,
+    partialProfit: 0,
+    partialLoss: 0,
+    averagePoints: 0,
     data: [
-      {
-        date: 0,
-        type: "Buy/Sell",
-        entry: 300,
-        stop: 300,
-        stopPercentage: 1,
-        takeProfit: 0,
-        closePrice: 0,
-        exitprice: 0,
-        profit: 1,
-        pointsEarnedAndLoss: 0,
-      },
-      {
-        date: 0,
-        type: "Buy/Sell",
-        entry: 300,
-        stop: 300,
-        stopPercentage: 1,
-        takeProfit: 0,
-        closePrice: 0,
-        exitprice: 0,
-        profit: 1,
-        pointsEarnedAndLoss: 0,
-      },
     ],
     touched: false,
   };
